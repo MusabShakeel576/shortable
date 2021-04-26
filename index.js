85092a2a7068737b7b0c5e3b96b6d92eb18317bd
MusabShakeel576 <musabshakeel576@gmail.com>
2021-04-26 22:23:52 +0500

    fourth

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const Discord =  require('discord.js');
 const client = new Discord.Client();
-const prefix = '!';
 client.once('ready', () => {
     console.log('Shortable is online!');
     // client.user.setActivity('musab.kedruga.com');
 });
 
 client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).split(/ +/);
+    if(message.author.bot) return;
+    const args = message.content.split(/ +/);
     const command = args.shift();
     const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     const number = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -16,7 +15,7 @@ client.on('message', message => {
         var lastValue = command.split("/").pop();
         const randomAlphabet = Math.floor(Math.random() * alphabet.length);
         const randomNumber = Math.floor(Math.random() * number.length);
-        var reply = "https://mintable.app/"+alphabet[randomAlphabet]+"/item/"+number[randomNumber]+"/"+lastValue
+        var reply = "https://mintable.app/"+alphabet[randomAlphabet]+"/item/"+number[randomNumber]+"/"+lastValue + " ("+message.author.username+" your new short URL, use it)"
         message.channel.send(reply);
     }
 });
