50a9872946ed14c8184a68a897a43c686f9db931
MusabShakeel576 <musabshakeel576@gmail.com>
2021-04-26 23:01:52 +0500

    fifth

--- index.js
+++ index.js
@@ -8,15 +8,17 @@ client.once('ready', () => {
 client.on('message', message => {
     if(message.author.bot) return;
     const args = message.content.split(/ +/);
-    const command = args.shift();
     const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     const number = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    if(command.startsWith("https://mintable.app/") === true || command.startsWith("https://www.mintable.app/") === true || command.startsWith("www.mintable.app/") === true || command.startsWith("mintable.app/") === true){
-        var lastValue = command.split("/").pop();
-        const randomAlphabet = Math.floor(Math.random() * alphabet.length);
-        const randomNumber = Math.floor(Math.random() * number.length);
-        var reply = "https://mintable.app/"+alphabet[randomAlphabet]+"/item/"+number[randomNumber]+"/"+lastValue + " ("+message.author.username+" your new short URL, use it)"
-        message.channel.send(reply);
+    args.forEach(messageSend);
+    function messageSend(item, index){
+        if(item.startsWith("https://mintable.app/") === true || item.startsWith("https://www.mintable.app/") === true || item.startsWith("www.mintable.app/") === true || item.startsWith("mintable.app/") === true){
+            var lastValue = item.split("/").pop();
+            const randomAlphabet = Math.floor(Math.random() * alphabet.length);
+            const randomNumber = Math.floor(Math.random() * number.length);
+            var reply = "https://mintable.app/"+alphabet[randomAlphabet]+"/item/"+number[randomNumber]+"/"+lastValue + " ("+message.author.username+" your new short URL, use it)"
+            message.channel.send(reply);
+        }
     }
 });
 
