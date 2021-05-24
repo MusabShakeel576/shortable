fa95c95de8ca9e66db24b070d0eeeeb80f2210c5
MusabShakeel576 <musabshakeel576@gmail.com>
2021-05-24 21:47:56 +0500

    Eleven

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ client.once('ready', () => {
 
 client.on('message', message => {
     if(message.author.bot) return;
+    let serverId = message.guild.id;
+    let channelId = message.channel.id;
     const args = message.content.split(/ +/);
     const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     const number = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -19,9 +21,12 @@ client.on('message', message => {
         else if(split[5] == 1 || split[5] == 2 || split[5] == 3 || split[5] == 4 || split[5] == 5 || split[5] == 6 || split[5] == 7 || split[5] == 8 || split[5] == 9){
             return
         }
+        else if(item.startsWith("https:") === true || item.startsWith("http:") === true){
+            if(serverId == '477603375077130242' && channelId == '477603375593291776'){
+                message.delete({timeout: 5000});
+            }
+        }
         else if(item.startsWith("https://mintable.app/") === true || item.startsWith("https://www.mintable.app/") === true || item.startsWith("www.mintable.app/") === true || item.startsWith("mintable.app/") === true){
-            let serverId = message.guild.id;
-            let channelId = message.channel.id;
             if(serverId == '477603375077130242'){
                 if(channelId == '827036035170828318' || channelId == '827036054301310986' || channelId == '827036068292853771' || channelId == '827036103592247327' || channelId == '827036146714017813' || channelId == '827036209159208991' || channelId == '833521055092047873' || channelId == '757781934058504262' || channelId == '757781979264450591'){
                     var lastValue = split.pop();
