630d97c69e732087af89acf528904fe92751f114
MusabShakeel576 <musabshakeel576@gmail.com>
2021-05-25 20:05:12 +0500

    Thirteen

--- index.js
+++ index.js
@@ -9,24 +9,30 @@ client.on('message', message => {
     if(message.author.bot) return;
     let serverId = message.guild.id;
     let channelId = message.channel.id;
-    let selfPromoReply = message.author.username+' No self promotion in this channel.';
+    let selfPromoReply = `<@${message.author.id}> No self promotion in this channel.`;
     const args = message.content.split(/ +/);
     const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     const number = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
     args.forEach(messageSend);
     function messageSend(item, index){
         const split = item.split("/");
-        if(split.length != 7){
-            return
-        }
-        else if(split[5] == 1 || split[5] == 2 || split[5] == 3 || split[5] == 4 || split[5] == 5 || split[5] == 6 || split[5] == 7 || split[5] == 8 || split[5] == 9){
-            return
-        }
-        else if(serverId == '477603375077130242' && channelId == '477603375593291776'){
+        if(serverId == '477603375077130242' && channelId == '477603375593291776'){
             if(item.startsWith("https:") === true || item.startsWith("http:") === true){
                 message.channel.send(selfPromoReply);
                 message.delete({timeout: 5000});
             }
+        }
+        else if(serverId == '653624979497156608' && channelId == '835113225066381342'){
+            if(item.startsWith("https:") === true || item.startsWith("http:") === true){
+                message.channel.send(selfPromoReply);
+                message.delete({timeout: 5000});
+            }
+        }
+        else if(split.length != 7){
+            return
+        }
+        else if(split[5] == 1 || split[5] == 2 || split[5] == 3 || split[5] == 4 || split[5] == 5 || split[5] == 6 || split[5] == 7 || split[5] == 8 || split[5] == 9){
+            return
         }
         else if(item.startsWith("https://mintable.app/") === true || item.startsWith("https://www.mintable.app/") === true || item.startsWith("www.mintable.app/") === true || item.startsWith("mintable.app/") === true){
             if(serverId == '477603375077130242'){
