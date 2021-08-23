1a7db5c94e070cf1f90636c5948b4af2b6ced34f
MusabShakeel576 <musabshakeel576@gmail.com>
2021-08-23 19:09:31 +0500

    [add] Tag users for new shorten url

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Shortable
+Long URLs are hard to share in limited character social media post (such as tweet in Twitter), and for modern era of NFT and Metaverse it's not user friendly, this is where Shortable come into play, a Discord bot build on top of server-side language Node JS to provide short and beautiful Mintable NFT URLs.
+---
+### Roadmap:
+Leaderboard to show users who have shorten their NFT URLs and users who spam the most.
--- index.js
+++ index.js
@@ -2,30 +2,47 @@ const Discord =  require('discord.js');
 const client = new Discord.Client();
 client.once('ready', () => {
     console.log('Shortable is online!');
-    // client.user.setActivity('musab.kedruga.com');
+    // client.user.setActivity('kedruga.com/shortable');
 });
 
 client.on('message', message => {
     if(message.author.bot) return;
+
     let serverId = message.guild.id;
     let channelId = message.channel.id;
+    let mode = "Production";
+    let ownerId;
+    let guildId;
+    let generalChannelId;
+    let openChannelId;
+
+    if(mode == "Staging") {
+        ownerId = "766333921151418428";
+        guildId = "879279892976504852";
+        generalChannelId = "879279892976504856";
+        openChannelId = ["879280149403664405", "879323022295711785"];
+    }else if(mode == "Production") {
+        ownerId = "202194883677519873";
+        guildId = "477603375077130242";
+        generalChannelId = "477603375593291776";
+        openChannelId = ["827036035170828318", "827036054301310986", "827036068292853771", "827036103592247327", "827036146714017813", "827036209159208991", "833521055092047873", "757781934058504262", "757781979264450591"];
+    }
+
     let selfPromoReply = `<@${message.author.id}> No self promotion in this channel.`;
+    let isChannelOpen = false;
     const args = message.content.split(/ +/);
     const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     const number = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
     args.forEach(messageSend);
     function messageSend(item, index){
         const split = item.split("/");
-        if(serverId == '477603375077130242' && channelId == '477603375593291776'){
-            if(item.startsWith("https:") === true || item.startsWith("http:") === true){
-                message.channel.send(selfPromoReply);
-                message.delete({timeout: 5000});
-            }
+        function restrictedChannel() {
+            message.channel.send(selfPromoReply);
+            message.delete({timeout: 5000});
         }
-        else if(serverId == '653624979497156608' && channelId == '835113225066381342'){
+        if(serverId == guildId && channelId == generalChannelId){
             if(item.startsWith("https:") === true || item.startsWith("http:") === true){
-                message.channel.send(selfPromoReply);
-                message.delete({timeout: 5000});
+                restrictedChannel();
             }
         }
         else if(split.length != 7){
@@ -35,40 +52,30 @@ client.on('message', message => {
             return
         }
         else if(item.startsWith("https://mintable.app/") === true || item.startsWith("https://www.mintable.app/") === true || item.startsWith("www.mintable.app/") === true || item.startsWith("mintable.app/") === true){
-            if(serverId == '477603375077130242'){
-                if(channelId == '827036035170828318' || channelId == '827036054301310986' || channelId == '827036068292853771' || channelId == '827036103592247327' || channelId == '827036146714017813' || channelId == '827036209159208991' || channelId == '833521055092047873' || channelId == '757781934058504262' || channelId == '757781979264450591'){
-                    var lastValue = split.pop();
-                    const randomAlphabet = Math.floor(Math.random() * alphabet.length);
-                    const randomNumber = Math.floor(Math.random() * number.length);
-                    var reply = "https://mintable.app/"+alphabet[randomAlphabet]+"/item/"+number[randomNumber]+"/"+lastValue + " ("+message.author.username+" your new short URL, use it)"
-                    message.channel.send(reply);
-                    message.delete({timeout: 5000});
-                }else{
-                    if(message.author.id != '202194883677519873'){
-                        message.channel.send(selfPromoReply);
-                        message.delete({timeout: 5000});
-                    }
-                }
-            }else if(serverId == '653624979497156608'){
-                if(channelId == '653624979497156611'){
-                    var lastValue = split.pop();
-                    const randomAlphabet = Math.floor(Math.random() * alphabet.length);
-                    const randomNumber = Math.floor(Math.random() * number.length);
-                    var reply = "https://mintable.app/"+alphabet[randomAlphabet]+"/item/"+number[randomNumber]+"/"+lastValue + " ("+message.author.username+" your new short URL, use it)"
-                    message.channel.send(reply);
-                    message.delete({timeout: 5000});
-                }else{
-                    if(message.author.id != '202194883677519873'){
-                        message.delete({timeout: 5000});
-                    }
-                }
-            }else{
+            function shortUrl() {
                 var lastValue = split.pop();
                 const randomAlphabet = Math.floor(Math.random() * alphabet.length);
                 const randomNumber = Math.floor(Math.random() * number.length);
-                var reply = "https://mintable.app/"+alphabet[randomAlphabet]+"/item/"+number[randomNumber]+"/"+lastValue + " ("+message.author.username+" your new short URL, use it)"
+                var reply = `https://mintable.app/${alphabet[randomAlphabet]}/item/${number[randomNumber]}/${lastValue} (<@${message.author.id}> your new short URL, use it)`;
                 message.channel.send(reply);
                 message.delete({timeout: 5000});
+            }
+            if(serverId == guildId){
+                openChannelId.forEach(openChannel);
+                function openChannel(item, index) {
+                    if(channelId == item) {
+                        isChannelOpen = true;
+                    }
+                }
+                if(isChannelOpen) {
+                    shortUrl();
+                }else {
+                    if(message.author.id != ownerId){
+                        restrictedChannel();
+                    }
+                }
+            }else{
+                shortUrl();
             }
         }
     }
